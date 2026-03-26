2eb899dddc5aa21ed7e073b3ecda3b97c5a01ed4
SKolev24 <104687591+SKolev24@users.noreply.github.com>
2026-03-26 16:30:41 +0000

    Update DNSrules.py

--- DetectionLogic/SpecificDetect/DNSrules.py
+++ DetectionLogic/SpecificDetect/DNSrules.py
@@ -3,15 +3,20 @@ from scapy.layers.dns import DNS
 from rich import print, console
 import math
 console = console.Console()
-
+domain_parts = []
+appearance={}
 def shannon_entropy(domain):
+    global domain_parts
+    domain_parts = domain.split(".")
+    domain = ".".join(part for part in domain.split(".") if part)
     entropy = 0
     p_x = []
     for c in set(domain):
         p_x = domain.count(c) / len(domain)
     entropy -= p_x * math.log(p_x, 2)
-    if entropy >= 0.2:
+    if entropy >= 0.45:
         console.print(f"[bold yellow]WARNING: suspicious entropy {domain}[/bold yellow]")
+        length_analysis(domain)
     print(f"[bold cyan]entropy: {entropy}[/bold cyan]")
 
 def length_analysis(domain):
@@ -22,15 +27,22 @@ def length_analysis(domain):
         pass
 
 def rarity_analysis(domain):
-    freq_analysis(domain)
-    pass
+        appearance[domain] = appearance.get(domain, 0) + 1
+        if appearance[domain] > 5:
+            freq_analysis(domain)
+        else
+            pass
 
 def freq_analysis(domain):
     isThreat = False
-    if isThreat:
-        console.print(f"[bold red] THREAD DISCOVERED: {domain}")
+    for domains in appearance:
+        if domain_parts[1] in appearance[domains] and domain_parts[0] != appearance[domains.split(".")[0]]:
+            isThreat = True
+        else:
+            pass
 
-    pass
+    if isThreat:
+        console.print(f"[bold red] THREAT DISCOVERED: {domain}")
 
 def dns_analysis_chain(packet,domain):
     console.print(f"[bold cyan]{domain}[/bold cyan]")
