64408f4661d7ff7eeb846dbd4c28d40d2de43b3f
SKolev24 <104687591+SKolev24@users.noreply.github.com>
2026-04-04 18:58:51 +0100

    fix argimport error

--- DetectionLogic/ScapyDetectionMethod.py
+++ DetectionLogic/ScapyDetectionMethod.py
@@ -5,4 +5,4 @@ from DetectionLogic.PacketRouter import *
 
 def sniff_packet(interface, args_silent, args_log):
     print(f"Listening on {interface}: ")
-    sniff(iface=interface, prn=lambda pkt: process_packet(pkt,args_silent,args_log))
+    sniff(iface=interface, prn=lambda pkt: process_packet(pkt,args_silent,args_log,False))
