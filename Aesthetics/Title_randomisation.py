6f259bf28c66db9bd52653e3c424e54763628eea
SKolev24 <104687591+SKolev24@users.noreply.github.com>
2026-04-04 19:18:17 +0100

    fix banner library error

--- Aesthetics/Title_randomisation.py
+++ Aesthetics/Title_randomisation.py
@@ -1,13 +1,24 @@
-from pyfiglet import Figlet
 from rich import print
-
 import random
 
-FONTS = ["slant", "big", "doom", "banner3-D", "ansi_shadow", "alligator2"]
-
-def banner(text="DEXED"):
-    font = random.choice(FONTS)
-    f = Figlet(font=font)
-    art = f.renderText(text)
-    print(f"[cyan]{art}[/cyan]")
-    print(f"[cyan]Data Exfiltration Detector[/cyan]")
+banners = [
+"""
+    ____  _______  __ __________
+   / __ \/ ____/ |/ // ____/ __ \\
+  / / / / __/  |   // __/ / / / /
+ / /_/ / /___ /   |/ /___/ /_/ /
+/_____/_____//_/|_/_____/_____/
+""",
+"""
+'########::'########:'##::::'##:'########:'########::
+ ##.... ##: ##.....::. ##::'##:: ##.....:: ##.... ##:
+ ##:::: ##: ##::::::::. ##'##::: ##::::::: ##:::: ##:
+ ##:::: ##: ######:::::. ###:::: ######::: ##:::: ##:
+ ##:::: ##: ##...:::::: ## ##::: ##...:::: ##:::: ##:
+ ##:::: ##: ##:::::::: ##:. ##:: ##::::::: ##:::: ##:
+ ########:: ########: ##:::. ##: ########: ########::
+........:::........::..:::::..::........::........:::
+"""
+]
+def banner():
+    print(f"[cyan]{random.choice(banners)}[/cyan]")
