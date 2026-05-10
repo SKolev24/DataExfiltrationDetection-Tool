1dee25e85ae625345b90675efe1c6d49fe636cd6
B00164905-tud <b00164905@mytudublin.ie>
2026-05-10 19:00:16 +0100

    .

--- Util/Entropy.py
+++ Util/Entropy.py
@@ -1,14 +1,20 @@
 import math
 
 def calculate_entropy(data):
+    # Stores how many times each letter appears
+    char_appears = {}
+    # Stores the final entropy value
     entropy = 0
-    freq = {}
 
-    for byte in data:
-        freq[byte] = freq.get(byte, 0) + 1
+    # Loops through each character in the string
+    for ch in data:
+        freq = char_appears.get(ch, 0)+ 1
 
+    # Loop goes through the frequency of each character
     for count in freq.values():
+        # Calculates the probability of each character
         p = count / len(data)
+        # Calculates the entropy of each character
         entropy -= p * math.log2(p)
-
+    # Returns the final entropy value
     return entropy
